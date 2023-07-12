879ffbfa728bd9b4d0265f32a09072a237f00806
xixue <2411579716@qq.com>
2023-07-12 22:00:06 +0800

    update webserver

---
+++ webserver/Util.cpp
@@ -0,0 +1,83 @@
+#include "Util.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+const int MAX_BUFF = 4096;
+ssize_t readn(int fd, void* buff, size_t n) {
+    size_t nleft = n;
+    ssize_t nread = 0;
+    ssize_t readSum = 0;
+    char* ptr = (char*)buff;
+    while (nleft > 0) {
+        if((nread = read(fd, ptr, nleft)) < 0) {
+            if(errno == EINTR) {
+                nread = 0;
+            } else if(errno == EAGAIN) {
+                return readSum;
+            } else {
+                return -1;
+            }
+        } else if(nread == 0) {
+            break;
+        }
+        readSum += nread;
+        nleft -= nread;
+        ptr += nread;
+    }
+    return readSum;
+}
+
+ssize_t readn(int fd, std::string &inBuffer, bool &zero) {
+    ssize_t nread = 0;
+    ssize_t readSum = 0;
+    while (true) {
+        char buff[MAX_BUFF];
+        if ((nread = read(fd, buff, MAX_BUFF)) < 0) {
+            if (errno == EINTR) {
+                continue;
+            } else if (errno == EAGAIN) {
+                return readSum;
+            } else {
+                perror("read error");
+                return -1;
+            }
+        } else if (nread == 0) {
+            zero = true;
+            break;
+        }
+        readSum += nread;
+        inBuffer += std::string(buff, buff + nread);
+    }
+    return readSum;
+}
+
+ssize_t readn(int fd, std::string &inBuffer) {
+    ssize_t nread = 0;
+    ssize_t readSum = 0;
+    while (true) {
+        char buff[MAX_BUFF];
+        if ((nread = read(fd, buff, MAX_BUFF)) < 0) {
+            if (errno == EINTR) {
+                continue;
+            } else if (errno == EAGAIN) {
+                return readSum;
+            } else {
+                perror("read error");
+                return -1;
+            }
+        } else if (nread == 0) {
+            break;
+        }
+        readSum += nread;
+        inBuffer += std::string(buff, buff + nread);
+    }
+    return readSum;
+}
---
+++ webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/song/webserver/WebServer/base/AsyncLogging.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/AsyncLogging.o"
+  "/home/song/webserver/WebServer/base/CountDownLatch.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/CountDownLatch.o"
+  "/home/song/webserver/WebServer/base/FileUtil.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/FileUtil.o"
+  "/home/song/webserver/WebServer/base/LogFile.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/LogFile.o"
+  "/home/song/webserver/WebServer/base/LogStream.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/LogStream.o"
+  "/home/song/webserver/WebServer/base/Logging.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/Logging.o"
+  "/home/song/webserver/WebServer/base/Thread.cpp" "/home/song/webserver/WebServer/base/build/CMakeFiles/libserver_base.dir/Thread.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
